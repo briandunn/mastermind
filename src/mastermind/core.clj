cf79a239570b950fd94842f272a581d7a5b92b94
Brian Dunn <dev+@bradsdeals.com>
2014-07-02 23:06:16 -0500

    Implement scoring

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -1,5 +1,8 @@
 (ns mastermind.core
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.math.combinatorics :refer [selections]]
+            [clojure.set :as set]
+            [clojure.test :refer :all]
+            ))
 
 (def colors
   {
@@ -11,20 +14,42 @@
    :v "[35m"
   })
 
-
 (def all-codes
-  (combo/selections (keys colors) 4))
+  (selections (vec (keys colors)) 4))
 
 (defn color-wrap [color s]
   (str \u001b (get colors color) (str s) (str \u001b "[0m")))
 
-(defn print-code [code]
-  (println (apply str (map #(color-wrap % "*") code))))
+(defn colorized-code [code]
+  (apply str (map #(color-wrap % "*") code)))
 
-(comment
-  (print-code (rand-nth all-codes))
-)
+(defn white-score [code guess]
+  (loop [c (frequencies code), g (frequencies guess), score 0]
+    (if c
+      (let [[color freq] (first c)]
+        (recur (next c), (dissoc g color), (+ score (min freq (or (color g) 0)))))
+      score)))
+
+(defn score [guess code]
+  (let [black (count (filter true? (map = guess code)))]
+  {
+   :black black
+   :white (#(if (< % 0) 0 %) (- (white-score guess code) black))
+  }
+))
 
 (defn -main
   []
-  (print-code (rand-nth all-codes)))
+  (let [secret (rand-nth all-codes)]
+    (println "--|" (colorized-code secret) "|--\n--| ---- |--")
+    (println (score [:r :r :y :y] secret))
+    (apply println (map colorized-code all-codes))
+    ))
+
+(deftest test-score
+  (is (= {:black 4, :white 0} (score [ :r :r :y :y ] [ :r :r :y :y ])))
+  (is (= {:black 0, :white 0} (score [ :b :b :b :b ] [ :r :r :y :y ])))
+  (is (= {:black 1, :white 0} (score [ :r :b :b :b ] [ :r :r :y :y ])))
+  (is (= {:black 0, :white 4} (score [ :y :y :r :r ] [ :r :r :y :y ])))
+  (is (= {:black 1, :white 2} (score [ :r :y :r :r ] [ :r :r :y :y ])))
+  )
