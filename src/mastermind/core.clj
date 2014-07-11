a951251519873ab81368053ff0a0a8cc285f9c56
Hashrocket Workstation <dev@hashrocket.com>
2014-07-11 16:33:51 -0500

    sequester formatting

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -13,62 +13,64 @@
    :v "[35m"
   })
 
-(def keypeg-colors
-  {
-   :black "[7m"
-   :white "[1m"
-  })
-
 (def all-codes
   (selections (vec (keys colors)) 4))
 
-(defn color-wrap [color s]
-  (str \u001b color (str s) (str \u001b "[0m")))
-
-(defn colorized-code [code]
-  (apply str (map #(color-wrap (% colors) "*") code)))
-
 (defn white-score [code guess]
-  (loop [c (frequencies code), g (frequencies guess), score 0]
+  (loop [c (frequencies code)
+         g (frequencies guess)
+         score 0]
     (if c
       (let [[color freq] (first c)]
-        (recur (next c), (dissoc g color), (+ score (min freq (or (color g) 0)))))
+        (recur
+          (next c)
+          (dissoc g color)
+          (+ score (min freq (or (color g) 0)))))
       score)))
 
 (defn score [guess code]
   (let [black (count (filter true? (map = guess code)))]
-  {
-   :black black
-   :white (max 0 (- (white-score guess code) black))
-  }
-))
-
-(defn score-characters [score]
- (apply concat (map (fn [[color count]] (repeat count (color-wrap (color keypeg-colors) "."))) score)))
+    {:black black
+     :white (max 0 (- (white-score guess code) black))}))
 
 (defn next-guess [last-guess s possible-guesses]
   (let [pg (filter #(= (score % last-guess) s) possible-guesses)]
     [(rand-nth pg) pg]))
 
+(defn sink [[topic message]]
+  (let [keypeg-colors { :black "[7m", :white "[1m" }
+        color-wrap (fn [color s]
+                     (str \u001b color (str s) (str \u001b "[0m")))
+        colorized-code (fn [code]
+                          (apply str (map #(color-wrap (% colors) "*") code)))
+        score-characters (fn [score]
+                           (apply concat (map (fn [[color count]]
+                                                (repeat count (color-wrap (color keypeg-colors) "."))) score)))]
+    (case topic
+      :secret (println (format "  | %s |  \n--| ---- |--" (colorized-code message)))
+      :row    (let [{:keys [guess possible score]} message
+                    [left right] (partition 2 2 (repeat " ") (score-characters score))]
+                (println (format "%2s| %s |%2s\t\t%-4s"
+                                (apply str left)
+                                (colorized-code guess)
+                                (apply str right)
+                                possible)))
+      :success (println (format "Success! %s guesses." message)))))
+
 (defn play []
   (let [secret (rand-nth all-codes)]
-    (comment (format "  | %s |  \n--| ---- |--" (colorized-code secret)))
+    (sink [:secret secret])
     (loop [guess [:r :r :y :y], possible-guesses (remove #{guess} all-codes), guess-count 1]
       (let [score (score guess secret)]
-        (let [[left right] (partition 2 2 (repeat " ") (score-characters score))]
-        (comment (format "%2s| %s |%2s\t\t%-4s"
-                        (apply str left)
-                        (colorized-code guess)
-                        (apply str right)
-                        (count possible-guesses)))
+        (sink [:row {:guess guess :possible (count possible-guesses) :score score}])
         (if (< (:black score ) 4)
           (let [[ng pgs] (next-guess guess score possible-guesses)]
             (recur ng, pgs, (inc guess-count)))
-          (do (comment (format "Success! %s guesses." guess-count)) guess-count)))))))
+          (do (sink [:success guess-count]) guess-count))))))
 
 (defn -main
   []
-  (time (let [games ( map (fn [i] (play)) (range 100))]
+  (time (let [games (map (fn [_] (play)) (range 100))]
     (println (format "games:\t%d\nworst:\t%d\nbest:\t%d\navg:\t%f"
                       (count games)
                       (apply max games)
