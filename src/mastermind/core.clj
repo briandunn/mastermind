96fd556356206adc7cd6fb0efea1f2ff7ae6bd42
Brian Dunn <brian@theophil.us>
2014-07-12 11:20:11 -0500

    super slow mini max

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -6,16 +6,21 @@
 
 (def colors
   {
-   :r "[31m"
-   :y "[33m"
-   :g "[32m"
-   :b "[34m"
-   :i "[36m"
-   :v "[35m"
+   :r 31
+   :y 33
+   :g 32
+   :b 34
+   :i 36
+   :v 35
   })
 
 (def all-codes
-  (selections (vec (keys colors)) 4))
+  (selections (set (keys colors)) 4))
+
+(def all-scores
+  (set (map (fn [f]
+              {:black (get f :black 0)
+               :white (get f :white 0)}) (map frequencies (selections [:black :white :empty] 4)))))
 
 (defn white-score [code guess]
   (let [shared (intersection (set code) (set guess))
@@ -27,14 +32,27 @@
     {:black black
      :white (max 0 (- (white-score guess code) black))}))
 
-(defn next-guess [last-guess s possible-guesses]
-  (let [pg (filter #(= (score % last-guess) s) possible-guesses)]
-    [(rand-nth pg) pg]))
+(defn mini-max [guesses]
+  (let [min-ranks (map (fn [guess]
+                         (apply min-key :rank (map (fn [s]
+                                                     {:rank (- (count guesses) (count (filter #(= (score % guess) s) guesses)))
+                                                      :guess guess
+                                                     }) all-scores))) guesses)]
+  (map :guess (val (apply max-key key (group-by :rank min-ranks))))))
+
+(mini-max (take 100 all-codes))
+
+(defn next-guess [last-guess s unplayed-guesses possible-guesses]
+  (let [pg (filter #(= (score % last-guess) s) possible-guesses)
+        mini-max (mini-max possible-guesses)
+        shared (intersection (set mini-max) (set pg))
+        next-guess (if (empty? shared) (rand-nth mini-max) (first shared))]
+    [next-guess pg]))
 
 (defn sink [[topic message]]
-  (let [keypeg-colors { :black "[7m", :white "[1m" }
+  (let [keypeg-colors { :black 7, :white 1 }
         color-wrap (fn [color s]
-                     (str \u001b color (str s) (str \u001b "[0m")))
+                     (str \u001b "[" color "m" s \u001b "[0m"))
         colorized-code (fn [code]
                           (apply str (map #(color-wrap (% colors) "*") code)))
         score-characters (fn [score]
@@ -54,17 +72,20 @@
 (defn play []
   (let [secret (rand-nth all-codes)]
     (sink [:secret secret])
-    (loop [guess [:r :r :y :y], possible-guesses (remove #{guess} all-codes), guess-count 1]
+    (loop [guess [:r :r :y :y]
+           unplayed-guesses (remove #{guess} all-codes)
+           possible-guesses unplayed-guesses
+           guess-count 1]
       (let [score (score guess secret)]
         (sink [:row {:guess guess :possible (count possible-guesses) :score score}])
         (if (< (:black score ) 4)
-          (let [[ng pgs] (next-guess guess score possible-guesses)]
-            (recur ng, pgs, (inc guess-count)))
+          (let [[ng pgs] (next-guess guess score unplayed-guesses possible-guesses)]
+            (recur ng (remove #{guess} unplayed-guesses) pgs (inc guess-count)))
           (do (sink [:success guess-count]) guess-count))))))
 
 (defn -main
   []
-  (time (let [games (map (fn [_] (play)) (range 100))]
+  (time (let [games (map (fn [_] (play)) (range 1))]
     (println (format "games:\t%d\nworst:\t%d\nbest:\t%d\navg:\t%f"
                       (count games)
                       (apply max games)
